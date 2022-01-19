3aae0b36fa304a3026be96621b7c39c91c2ba652
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-01-19 15:58:45 -0500

    created interviewerList

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,19 @@
+import React from "react";
+import "components/InterviewerListItem.scss";
+import classnames from "classnames";
+
+export default function InterviewerListItem(props) {
+  const selectedInterviewer = classnames("interviewers__item", {
+    "interviewers__item--selected": props.selected
+  });
+  return (
+    <li className={selectedInterviewer} onClick={props.setInterviewer}>
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
