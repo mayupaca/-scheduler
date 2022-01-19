94141ca2bc308e3a779898dddaeaff82f9575ac0
Mayu Suzumura <mayumurasuzu@gmail.com>
2022-01-18 20:59:18 -0500

    created DayList

---
+++ src/components/DayList.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+import "components/DayListItem.scss";
+import DayListItem from "components/DayListItem";
+
+export default function DayList(props) {
+  return (
+    <ul>
+      {props.days.map(day => {
+        return (
+          <DayListItem
+            key={day.id}
+            name={day.name}
+            spots={day.spots}
+            full={day.spots === 0}
+            selected={day.name === props.day}
+            setDay={props.setDay}
+          />
+        );
+      })}
+    </ul>
+  );
+}
